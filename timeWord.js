4bb00187e25293e3a4f4f75f69f30941518b1d5a
Rijo George <rijogeorge@rijos-mbp-2.lan>
2022-02-08 12:47:09 -0800

    Part 2: Timeword.js done

--- timeWord.js
+++ timeWord.js
@@ -1,5 +1,32 @@
 function timeWord(time) {
-
+    if(time === "00:00") return "midnight";
+    else if(time === "12:00") return "noon";
+    let ampm = "am"
+    const times = time.split(':')
+    let hour = parseInt(times[0])
+    const mins = parseInt(times[1])
+    if(hour >= 12) {
+        ampm = "pm"
+    }
+    const hourArray = ['twelve', 'one', 'two', 'three', 'four','five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve']
+    const lowNumArray = ['o’clock', 'oh one', 'oh two', 'oh three', 'oh four', 'oh five', 'oh six', 'oh seven', 'oh eight', 'oh nine',
+                            'ten', 'eleven', 'twelve', 'thierteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const highNumbers = ["twenty", "thirty", "fourty", "fifty"]
+    if(hour > 12) {
+        hour = hour - 12;
+    }
+    let wordTime=hourArray[hour]
+    if (mins<20){
+        wordTime += ` ${lowNumArray[mins]}`
+    } else {
+        const highNumberArrIdx = (Math.floor(mins/10))-2;
+        wordTime += ` ${highNumbers[highNumberArrIdx]}`
+        const lowNumberArrIdx = mins % 10;
+        if(lowNumberArrIdx !== 0) {
+            wordTime += ` ${hourArray[lowNumberArrIdx]}`
+        }
+    }
+     return wordTime+" "+ampm;
 }
 
 module.exports = timeWord
--- timeWord.test.js
+++ timeWord.test.js
@@ -4,4 +4,49 @@ describe('#timeword', () => {
   test('it is a function', () => {
     expect(typeof timeWord).toBe('function');
   });
+
+  test("00:00 returns mid night", () => {
+    expect(timeWord("00:00")).toBe("midnight")
+  })
+
+  test("12:00 returns mid noon", () => {
+    expect(timeWord("12:00")).toBe("noon")
+  })
+
+  test("00:12 returns twelve twelve am", () => {
+    expect(timeWord("00:12")).toBe("twelve twelve am")
+  })
+
+  test("01:00 returns one o’clock am", () => {
+    expect(timeWord("01:00")).toBe("one o’clock am")
+  })
+
+  test("06:01 returns six oh one am", () => {
+    expect(timeWord("06:01")).toBe("six oh one am")
+  })
+
+  test("06:10 returns six ten am", () => {
+    expect(timeWord("06:10")).toBe("six ten am")
+  })
+
+  test("06:18 returns six eighteen am", () => {
+    expect(timeWord("06:18")).toBe("six eighteen am")
+  })
+
+  test("06:30 returns six thirty am", () => {
+    expect(timeWord("06:30")).toBe("six thirty am")
+  })
+
+  test("10:34 returns ten thirty four am", () => {
+    expect(timeWord("10:34")).toBe("ten thirty four am")
+  })
+
+  test("12:09 returns twelve oh nine pm", () => {
+    expect(timeWord("12:09")).toBe("twelve oh nine pm")
+  })
+
+  test("23:23 returns eleven twenty three pm", () => {
+    expect(timeWord("23:23")).toBe("eleven twenty three pm")
+  })
+
 });
